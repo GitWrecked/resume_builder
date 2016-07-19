59ad6fab99a7232974d85e34bb05eafb1ac61951
Arun Varghese <avarghese@spireon.com>
2016-07-19 12:05:42 -0500

    dashboard for actions

---
+++ client/src/app/actions/actionsRoutes.js
@@ -0,0 +1,16 @@
+// actions routes ===================
+// angular routes using ui.router, allows for nested views
+angular.module('rbApp').config([
+    '$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('actions', {
+                url: '/actions',
+                templateUrl: 'src/app/actions/actionsTmpl.html'
+            })
+            .state('admin.actions', {
+                url: '/actions',
+                templateUrl: 'src/app/actions/actionsTmpl.html'
+            });
+    }
+]);
--- client/src/app/nav/navCtrl.js
+++ client/src/app/nav/navCtrl.js
@@ -2,8 +2,8 @@
 // angular controller to handle nav, including routes to call backend api
 // front end does not connect to mongo libraries, therefore api routes needed
 angular.module('rbApp').controller('navCtrl', [
-    '$scope', '$state', 'authSvc', '$mdDialog', '$mdBottomSheet', '$log',
-    function($scope, $state, authSvc, $mdDialog, $mdBottomSheet, $log) {
+    '$scope', '$state', 'authSvc', '$mdDialog', '$log',
+    function($scope, $state, authSvc, $mdDialog, $log) {
         $scope.currentUser = authSvc.user();
         $scope.$watch(authSvc.user, function() {
             $scope.currentUser = authSvc.user();
@@ -13,23 +13,21 @@ angular.module('rbApp').controller('navCtrl', [
             authSvc.logout();
             $state.go('login');
         };
-        $scope.openMenu = function($mdOpenMenu, ev) {
-            originatorEv = ev;
-            $mdOpenMenu(ev);
-        };
-        $scope.showActions = function() {
-            $scope.alert = '';
-            $mdBottomSheet.show({
-                templateUrl: 'src/app/actions/actionsTmpl.html',
-                clickOutsideToClose: true
-            });
-        };
         $scope.isAdmin = function() {
             return $scope.currentUser.admin;
         };
-
         $scope.isAuthenticated = function() {
             return $scope.currentUser;
         };
+        $scope.actions = [{
+            name: 'Users',
+            sref: 'dashboard'
+        }, {
+            name: 'Resumes',
+            sref: 'resumes'
+        }, {
+            name: 'Messages',
+            sref: 'messages'
+        }];
     }
 ]);
